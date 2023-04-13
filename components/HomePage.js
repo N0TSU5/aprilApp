0dc2432ec28dcf7f6c64d53420a8c52e4397dbfc
N0TSU5 <marten@panchev.com>
2023-04-14 00:06:50 +0100

    modal added

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage';
 import moment from 'moment-timezone';
+import RenderHTML from 'react-native-render-html';
 import PouchDB from 'pouchdb-react-native';
 import "../ignoreWarnings";
+import { useWindowDimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PurpleLogo from '../assets/greyLogo.png';
 import {
@@ -11,11 +13,32 @@ import {
     Text,
     TouchableOpacity,
     StyleSheet,
+    Modal
 } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
+
+const WebDisplay = React.memo(function WebDisplay({ html }) {
+    const { width: contentWidth } = useWindowDimensions();
+    const tagsStyles = {
+        a: {
+            textDecorationLine: 'none',
+        },
+    };
+
+    return (
+        <RenderHTML
+            contentWidth={contentWidth}
+            source={{ html }}
+            tagsStyles={tagsStyles}
+            ignoredDomTags={['font']}
+        />
+    );
+});
 
 const HomePage = () => {
 
     const navigation = useNavigation();
+    const { width } = useWindowDimensions();
 
     const clearData = async () => {
         try {
@@ -42,7 +65,10 @@ const HomePage = () => {
     const [hrDiff, setHrDiff] = useState();
     const [sHrDiff, setSHDiff] = useState();
     const [partOfDay, setDayPart] = useState();
-    const [itinerary, setList] = useState([])
+    const [itinerary, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [item, setItem] = useState([])
 
     useEffect(() => {
         const db = new PouchDB('userDB');
@@ -54,18 +80,28 @@ const HomePage = () => {
                 setTourName(firstDoc.data.tourname);
                 const itineraryObj = firstDoc.data.itinerary
                 setList(Object.keys(itineraryObj).map(key => itineraryObj[key]))
+                setIsLoading(false);
             })
             .catch((err) => {
                 console.error("home page error", err);
-            }); 
+            });
+
+        db.allDocs({ limit: 1, include_docs: true })
+            .then((result) => {
+                const firstDoc = result.rows[0].doc;
+                const itineraryObj = firstDoc.data.itinerary
+                setList(itineraryObj)
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.error("home page error", err);
+            });
+
     }, []);
-    useEffect(() => {
-        console.log("ititn",itinerary);
-      }, [itinerary]);
 
     useEffect(() => {
 
-        const date1 = moment.tz('2019-10-03 23:58 GMT', 'YYYY-MM-DD HH:mm z', 'GMT')
+        const date1 = moment.tz('2019-10-06 10:58 GMT', 'YYYY-MM-DD HH:mm z', 'GMT')
         const date2 = moment.tz(departure, 'GMT')
         const dateE = moment.tz(returned, 'GMT')
 
@@ -95,31 +131,57 @@ const HomePage = () => {
 
     }, [[returned, departure]]);
 
-    const formattedList = []
-    /*useEffect(() => {
+    const renderModal = (day) => {
+        const modalList = Object.keys(itinerary).map(key => itinerary[key])
 
-        const currentItem = itinerary[diff]
-        const currentLocation = currentItem.description
-        const itemDate = moment(currentItem.datestart).format('dddd D MMMM')
-        const itemIndex = diff + 1
-        const itemFooter = (currentItem.footer === null) ? '' : currentItem.footer
+        const groupedItinerary = [];
+        if (modalList.length > 0) {
+            let currentGroup = [modalList[0]];
+            let currentDate = new Date(modalList[0].datestart);
 
-        let itemDescription = ''
-        for (let j = 0; j < currentItem.length; j++) {
-            const cDesc = currentItem[j].description
-            if (cDesc !== currentLocation) {
-                itemDescription += cDesc
-            } else if (cDesc.length > 30) {
-                itemDescription += cDesc
+            for (let i = 1; i < modalList.length; i++) {
+                const item = modalList[i];
+                const itemDate = new Date(item.datestart);
+
+                if (itemDate.getTime() === currentDate.getTime()) {
+                    currentGroup.push(item);
+                } else {
+                    groupedItinerary.push(currentGroup);
+                    currentGroup = [item];
+                    currentDate = itemDate;
+                }
+            }
+            groupedItinerary.push(currentGroup);
+        }
+        console.log(groupedItinerary[0])
+        const formattedList = []
+
+        for (let i = 0; i < groupedItinerary.length; i++) {
+
+            const currentItem = groupedItinerary[i]
+            const currentLocation = currentItem[0].description
+            const itemDate = moment(currentItem[0].datestart).format('dddd D MMMM'); console.log(currentItem.datestart)
+            const itemIndex = i + 1
+            const itemFooter = (currentItem[0].footer === null) ? '' : currentItem[0].footer
+
+            let itemDescription = ''
+            for (let j = 0; j < currentItem.length; j++) {
+                const cDesc = currentItem[j].description
+                if (cDesc !== currentLocation) {
+                    itemDescription += cDesc
+                } else if (cDesc.length > 30) {
+                    itemDescription += cDesc
+                }
+            }
+            if (currentLocation.length < 30) {
+                formattedList.push([itemIndex, itemDate, currentLocation, itemDescription, itemFooter])
+            } else {
+                formattedList.push([itemIndex, itemDate, "", itemDescription, itemFooter])
             }
         }
-
-        if (currentLocation.length < 30) {
-            formattedList.push([itemIndex, itemDate, currentLocation, itemDescription, itemFooter])
-        } else {
-            formattedList.push([itemIndex, itemDate, "", itemDescription, itemFooter])
-        }
-    }, [itinerary, diff])*/
+        setItem(formattedList[day])
+        setModalVisible(true)
+    }
 
     const dayNoun = (diff == 1) ? 'day' : 'days';
     const hourPhrase = (diff > 0) ? `in ${diff} ${dayNoun}` : "tomorrow"
@@ -127,6 +189,29 @@ const HomePage = () => {
     return (
 
         <View style={containerStyles.container}>
+
+            <Modal
+                visible={modalVisible}
+                animationType='fade'
+                transparent={true}
+            >
+                <View style={modalStyles.modalContainer}>
+                    <ScrollView>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={{ fontWeight: 'bold', color: '#660033' }}>Day {item[0]}: {item[1]}</Text>
+                        </View>
+                        <RenderHTML source={{ html: item[2] }} baseStyle={{ fontWeight: 'bold', color: '#660033' }} contentWidth={width} />
+                        <React.Fragment>
+                            <WebDisplay html={item[3]} />
+                            <WebDisplay html={item[4]} />
+
+                        </React.Fragment>
+                        <TouchableOpacity onPress={() => setModalVisible(false)}>
+                            <Text style={{ color: 'red', fontSize: 20 }}>{'\n'}Close itinerary</Text>
+                        </TouchableOpacity>
+                    </ScrollView>
+                </View>
+            </Modal>
 
             {relative == "pre" && (
                 <>
@@ -139,7 +224,7 @@ const HomePage = () => {
                 <>
                     <Text style={greetStyles.greeting}>Good {partOfDay}, </Text>
                     <Text style={greetStyles.countdown}>Day {Math.abs(diff) + 1} of {tourname}</Text>
-                    <TouchableOpacity style={greetStyles.viewDoc}>
+                    <TouchableOpacity style={greetStyles.viewDoc} onPress={() => renderModal(Math.abs(diff))}>
                         <Text style={greetStyles.viewDocText}>View today's itinerary</Text>
                     </TouchableOpacity>
                 </>
@@ -154,6 +239,15 @@ const HomePage = () => {
     )
 }
 
+const modalStyles = StyleSheet.create({
+    modalContainer: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        padding: 20,
+        width: '80%',
+        alignSelf: 'center',
+    },
+})
 
 const greetStyles = StyleSheet.create({
     greeting: {
