55697c34d141d362c0eb2114e4c06793b6c710e7
N0TSU5 <marten@panchev.com>
2023-04-22 22:38:05 +0100

    gfdgh

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
 import { Text, View, StyleSheet } from 'react-native';
-import Geocoding from '@react-native-geocoding/geocoding';
 import PouchDB from 'pouchdb-react-native';
 
 const Map = () => {
@@ -30,23 +29,20 @@ const Map = () => {
     hotels.push({ name: name, address: address, phone: phone })
   }
 
-  const markers = []
-  for(let hotel in hotels){
+  const [markers, setMarkers] = useState([])
+  for (let hotel in hotels) {
     const address = hotel.address
-    Geocoding.from(address).then((response) => {
-      const { lat, lng } = response.results[0].geometry.location;
-      console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-    }).catch((error) => {
-      console.log(error);
-    });
-    
+
   }
 
   console.log(hotels)
 
   return (
-    <Text>morbb</Text>
+    <MapView
+        provider={PROVIDER_GOOGLE}
+    />
+
   )
-  }
+}
 
 export default Map
